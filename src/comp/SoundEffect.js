ad00addd028b4f861072fe1715aaf21ee04eb5c6
Victorowsky <Victorowskyy@gmail.com>
2020-12-30 14:43:15 +0100

    sound fix

--- src/comp/SoundEffect.js
+++ src/comp/SoundEffect.js
@@ -5,6 +5,7 @@ const SoundEffect = ({ isPlaying }) => {
   return (
     <ReactPlayer
       playing={isPlaying}
+      volume={1}
       style={{ display: "none" }}
       url="https://www.youtube.com/watch?v=rN4Y7mGlH8c"
     />
